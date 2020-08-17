5c30834a91149a4391947855368cdea7a0ae99ad
Max D - Alien <max@417max.com>
2020-08-17 05:29:41 -0400

    Initial Int2

--- KeyboardAutoTyper.py
+++ KeyboardAutoTyper.py
@@ -5,10 +5,33 @@ try:
     print("Running Secure Keyboard Shortcuter")
 
     #Adding a shortcut's
-    keyboard.add_abbreviation('@@', 'maxderevencha@gmail.com')
-    #keyboard.add_abbreviation('randompass', 'Open ')
+    keyboard.add_abbreviation('@1', 'maxderevencha@gmail.com')
+    print('@1', ' : maxderevencha@gmail.com')
+    
+    keyboard.add_abbreviation('@2', 'max@hacked.fyi')
+    print('@2', ' : max@hacked.fyi')
+    
+    keyboard.add_abbreviation('@3', 'max@callitclosed.com')
+    print('@3', ' : max@callitclosed.com')
+    
+    
     keyboard.add_abbreviation('2898', '2898 Gipper Circle, Sanford Florida, 32773')
+    print('2898', ' : 2898 Gipper Circle')
+    
     keyboard.add_abbreviation('417', '417-522-8598')
+    print('417', ' : 417-522-8598')
+    
+    keyboard.add_abbreviation('teh', 'the')
+    keyboard.add_abbreviation('Teh', 'The')
+    
+    keyboard.add_abbreviation('MD', 'Maksim Derevencha')
+    print('MD', ' : Maksim Derevencha')
+    
+    keyboard.add_abbreviation('mdlin', 'www.linkedin.com/in/max-derevencha')
+    print('mdlin', ' : www.linkedin.com/in/max-derevencha')
+    
+    keyboard.add_abbreviation('gv', '407-900-3908')
+    print('gv', ' : 407-900-3908')
     
     #Run until closed sorta like "While True"
     keyboard.wait()
@@ -16,3 +39,4 @@ try:
 #Exceptions just in case.
 except Exception as e:
     print(e)
+    input('')
